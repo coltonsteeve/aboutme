43a9855d9981c2d89f094392064a705de136e66a
coltonsteeve <coltonsteeve@gmail.com>
2019-02-18 12:25:35 -0600

    Added qunitTest.html and added it to the nav bar on all pages.

---
+++ js/qunitTest.js
@@ -0,0 +1,15 @@
+function tempConverter(number) {
+    valNum = parseFloat(number);
+    fToC = (number - 32) / 1.8;
+    return fToC;
+    //document.getElementById("celsius").innerHTML = (number-32) / 1.8;
+  }
+
+  QUnit.test("tempConverter", function(assert) {
+	assert.equal(tempConverter(32), 0, "32 degrees Fahrenheit == 0 degrees Celsius");
+	assert.equal(tempConverter(23), -5, "23 degrees Fahrenheit == -5 degrees Celsius");
+	assert.equal(tempConverter(50), 10, "50 degrees Fahrenheit == 10 degrees Celsius");
+	assert.equal(tempConverter(5), -15, "5 degrees Fahrenheit == -15 degrees Celsius");
+	assert.equal(tempConverter(500), 260, "500 degrees Fahrenheit == 260 degrees Celsius");
+	assert.equal(tempConverter('c'), undefined, "Cannot enter a character");
+ });
